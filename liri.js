e10199fa316ce520b95397b3b3209e89fe3fd8ab
oznero <kcroom@gmail.com>
2018-12-06 19:32:15 -0500

    updated files

--- liri.js
+++ liri.js
@@ -1,37 +1,129 @@
 require('dotenv').config();
-const fs = require('fs');
+let fs = require('fs');
 const request = require('request');
 let keys = require('./keys.js');
-var Spotify = require('node-spotify-api');
+let Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
 
-/*
-search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
 
-concert-this
-spotify-this-song
-movie-this
-do-what-it-says
-*/
-if (process.argv[2] === 'spotify-this-song') {
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+function getSpotifySong(secondParam) {
+    if (process.argv[3]) {
+        spotify.search({ type: 'track', query: process.argv[3] }, function (err, data) {
+            let artists = '';
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
 
-        console.log(data);
-    });
+            body = data.tracks.items[0];
+            body.artists.forEach(artist => {
+                artists += artist.name;
+            });
+            console.log(`Artist(s): ${artists}`);
+            console.log(`Song Name: ${body.name}`);
+            console.log(`Preview Link: ${body.external_urls.spotify}`);
+            console.log(`Album: ${body.album.name}`);
+        });
+    }
+        if (secondParam) {
+            spotify.search({ type: 'track', query: secondParam }, function (err, data) {
+                let artists = '';
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+    
+                body = data.tracks.items[0];
+                body.artists.forEach(artist => {
+                    artists += artist.name;
+                });
+                console.log(`Artist(s): ${artists}`);
+                console.log(`Song Name: ${body.name}`);
+                console.log(`Preview Link: ${body.external_urls.spotify}`);
+                console.log(`Album: ${body.album.name}`);
+            });
+    } else {
+        spotify.search({ type: 'track', query: "What's My Age Again" }, function (err, data) {
+            let artists = '';
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            body = data.tracks.items[0];
+            body.artists.forEach(artist => {
+                artists += artist.name;
+            });
+            console.log(`Artist(s): ${artists}`);
+            console.log(`Song Name: ${body.name}`);
+            console.log(`Preview Link: ${body.external_urls.spotify}`);
+            console.log(`Album: ${body.album.name}`);
+        });
+    }
 }
-if (process.argv[2] === 'concert-this') {
+
+function bandsintown() {
     request(`https://rest.bandsintown.com/artists/${process.argv[3]}/events?app_id=codingbootcamp`, (error, response, body) => {
         if (!error && response.statusCode === 200) {
 
             let response = JSON.parse(body);
             for (let i = 0; i < response.length; i++) {
-                console.log(`Venue name: ${response[i].venue.name}`);
-                console.log(`Venue city: ${response[i].venue.city}`);
+                console.log(`\nVenue name: ${response[i].venue.name}`);
+                console.log(`Venue city: ${response[i].venue.city}\n`);
             }
         }
 
+    });
+}
+function movieThis(){
+    if (process.argv[3]) {
+        request(`http://www.omdbapi.com/?apikey=trilogy&t=${process.argv[3]}`, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                let response = JSON.parse(body);
+                console.log(`\nMovie Title: ${response.Title}`);
+                console.log(`IMDB Rating: ${response.Ratings[0].Value}`);
+                console.log(`Rotten tomatoes rating ${response.Ratings[1].Value}`);
+                console.log(`Movie production country: ${response.Country}`);
+                console.log(`Movie language: ${response.Language}`);
+                console.log(`Plot of the movie: ${response.Plot}`);
+                console.log(`Actors in movie: ${response.Actors}`);
+            }
+        });
+    } else {
+        request(`http://www.omdbapi.com/?apikey=trilogy&t=Mr. Nobody`, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                let response = JSON.parse(body);
+                console.log(`\nMovie Title: ${response.Title}`);
+                console.log(`IMDB Rating: ${response.Ratings[0].Value}`);
+                console.log(`Rotten tomatoes rating ${response.Ratings[1].Value}`);
+                console.log(`Movie production country: ${response.Country}`);
+                console.log(`Movie language: ${response.Language}`);
+                console.log(`Plot of the movie: ${response.Plot}`);
+                console.log(`Actors in movie: ${response.Actors}`);
+            }
+        });
+    }
+
+}
+
+if (process.argv[2] === 'spotify-this-song') {
+    getSpotifySong();
+}
+if (process.argv[2] === 'concert-this') {
+    bandsintown();
+}
+if (process.argv[2] === 'movie-this') {
+    movieThis();
+}
+if (process.argv[2] === 'do-what-it-says') {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        const dataList = data.split(',');
+        const firstParam = dataList[0].trim();
+        const secondParam = dataList[1].trim();
+        if (firstParam === 'spotify-this-song') {
+            getSpotifySong(secondParam);
+        }
     });
 }
